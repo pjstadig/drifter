e0cf3afde2ca0acb533a348c4dc1993bd845c234
Paul Stadig <paul@stadig.name>
2016-05-18 06:48:12 -0400

    Use clojars for deployment.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/pjstadig/wayfarer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.6.0"]]
   :test-selectors {:default :postgresql
