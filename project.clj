21e0e52195a5e3ece89b98096b4189c78e54b1f4
Paul Stadig <paul@stadig.name>
2016-05-18 05:53:11 -0400

    Fix project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pjstadig/wayfarer "0.1.0-SNAPSHOT"
   :description "A simple, programmable migration library."
-  :url "https://github.com/pjstadig/drifter"
+  :url "https://github.com/pjstadig/wayfarer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
