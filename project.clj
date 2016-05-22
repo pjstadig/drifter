0bde3329cefc9f1409593e2b126705aa6af0f73d
Paul Stadig <paul@stadig.name>
2016-05-22 13:51:22 -0400

    Version 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pjstadig/wayfarer "0.2.0"
+(defproject pjstadig/wayfarer "0.2.1-SNAPSHOT"
   :description "A simple, programmable migration library."
   :url "https://github.com/pjstadig/wayfarer"
   :license {:name "Eclipse Public License"
